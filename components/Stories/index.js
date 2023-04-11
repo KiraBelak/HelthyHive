3eeedc12a8d7d712518f921f9701a722e9e2aeb2
Josué Garza <A281876@alumnos.uaslp.mx>
2023-04-11 09:19:57 -0600

    text changes

--- components/Stories/index.js
+++ components/Stories/index.js
@@ -9,7 +9,7 @@ const StoryImage = ({ imageSrc, altText, text }) => {
                 className="rounded-full object-cover absolute inset-0 h-full w-full"
             />
             <div className="absolute bottom-0 right-0 h-5 w-5 bg-yellow-500 border-2 border-white rounded-md flex justify-center items-center">
-                <i className="material-icons text-white text-xs">whatshot</i>
+                <i className="material-icons text-white text-xs"></i>
             </div>
             <div className="mt-1 text-center">
                 <p className="text-lg text-gray-700">{text}</p>
