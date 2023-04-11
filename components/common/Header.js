36a348f85f048cb318a8ff68462fd09401bf8b55
KiraBelak <kalebchivas@hotmail.com>
2023-04-11 03:14:21 -0600

    avatar sin login

--- components/common/Header.js
+++ components/common/Header.js
@@ -5,6 +5,12 @@ import { Popover, Transition, Menu } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import { useSession, signOut } from "next-auth/react";
 import classNames from "@/utils/classNames";
+import DefAvatar from "../users/avatar";
+
+
+
+
+
 
 //HEADER SETUP
 const logoUrl = "/logo.png";
@@ -17,17 +23,19 @@ const navigation = {
 
 const Header = () => {
   const { data: session } = useSession();
+//this is the random avatar generator
+
 
   return (
-    <Popover className="relative bg-white">
+    <Popover className="relative bg-salud-primary">
 
       <div
         className="absolute inset-0 shadow z-20 pointer-events-none"
         aria-hidden="true"
       />
-      <div className="relative z-20">
+      <div className="relative h-[150px] z-20">
         {/* DESKTOP */}
-        <div className="max-w-7xl mx-auto flex justify-between items-center px-4 py-5 sm:px-6 sm:py-4 lg:px-8 md:justify-start md:space-x-10">
+        <div className="max-w-7xl mx-auto flex-wrap flex justify-around items-center px-4 py-5 sm:px-6 sm:py-4 lg:px-8 md:justify-start md:space-x-10">
         {session ? (
                 <Menu as="div" className="ml-3 relative">
                   <div>
@@ -108,177 +116,23 @@ const Header = () => {
                 </Menu>
               ) : (
                 <Link href="/auth/signin">
-                  <a>Iniciar Sesión</a>
+                  <a>
+            <DefAvatar/>
+   </a>
                 </Link>
               )}
-          <div className="-mr-2 -my-2 md:hidden">
-            <Popover.Button className="bg-white
- rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-              <span className="sr-only">Open menu</span>
-              <MenuIcon className="h-6 w-6" aria-hidden="true" />
-            </Popover.Button>
-          </div>
-          <div className="hidden md:flex-1 md:flex md:items-center md:justify-between">
-            <Popover.Group as="nav" className="flex space-x-10">
-              {navigation.pages.map((page) => (
-                <Link key={page.name} href={page.href}>
-                  <a className="text-base font-medium text-[#fff] hover:text-gray-900">
-                    {page.name}
-                  </a>
-                </Link>
-              ))}
-            </Popover.Group>
-            {/* HEADER DEKTOP RIGHT SECTION BUTTONS */}
-            <div className="flex items-center md:ml-12">
-              {session ? (
-                  <Link key="buenas" href="/buenas">
-                  <a className="text-base font-medium text-[#fff] hover:text-gray-900">
-                    Progreso
-                  </a>
-                </Link>
-                ) : (
-                  <div>
-                    </div>
-                  )}
-          
-              {session ? (
-                <Menu as="div" className="ml-3 relative">
-                  <div>
-                    <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-offset-2  focus:ring-white">
-                      <span className="sr-only">Open user menu</span>
-                      {session.user.image ? (
-                        <img
-                          className="h-8 w-8 rounded-full"
-                          src={session.user.image}
-                          alt=""
-                        />
-                      ) : (
-                        <img
-                          className="h-8 w-8 rounded-full"
-                          src={`https://avatars.dicebear.com/api/micah/${session.user.email}.svg?background=%23ffffff`}
-                          alt=""
-                        />
-                      )}
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-gradient-to-r from-red-700 to-red-900 ring-1 ring-black ring-opacity-5 focus:outline-none z-40">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link href="/user/profile">
-                            <a
-                              className={classNames(
-                                active ? "bg-gray-100" : "",
-                                "block px-4 py-2 text-sm text-[#fff]"
-                              )}
-                            >
-                              Mi Cuenta
-                            </a>
-                          </Link>
-                        )}
-                      </Menu.Item>
-
-                      {session.user.roles.includes("admin") && (
-                        <Menu.Item>
-                          {({ active }) => (
-                            <Link href="/admin/dashboard">
-                              <a
-                                className={classNames(
-                                  active ? "bg-gray-100" : "",
-                                  "block px-4 py-2 text-sm text-[#fff]"
-                                )}
-                              >
-                                Admin Dashboard
-                              </a>
-                            </Link>
-                          )}
-                        </Menu.Item>
-                      )}
-
-                      <Menu.Item>
-                        {({ active }) => (
-                          <div
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-[#fff] cursor-pointer"
-                            )}
-                            onClick={() => signOut()}
-                          >
-                            Salir
-                          </div>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
-              ) : (
-                <Link href="/auth/signin">
-                  <a className="text-white">Iniciar Sesión</a>
-                </Link>
-              )}
-            </div>
-           
-          </div>
-        </div>
-      </div>
-
-      {/* MOBILE */}
-      <Transition
-        as={Fragment}
-        enter="duration-200 ease-out"
-        enterFrom="opacity-0 scale-95"
-        enterTo="opacity-100 scale-100"
-        leave="duration-100 ease-in"
-        leaveFrom="opacity-100 scale-100"
-        leaveTo="opacity-0 scale-95"
-      >
-        <Popover.Panel
-          focus
-          className="absolute z-30 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-        >
-          <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-            <div className="pt-5 pb-6 px-5 sm:pb-8">
-              <div className="flex items-center justify-between">
-                <div>
-                  <Link href="/">
-                    <a>
-                      {" "}
-                      <img className="h-8 w-auto" src={logoUrl} alt="logo" />
-                    </a>
-                  </Link>
-                </div>
-                <div className="flex items-center md:ml-12">
-             
-            </div>
-                <div className="-mr-2">
-                  <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                    <XIcon className="h-6 w-6" aria-hidden="true" />
-                  </Popover.Button>
-                </div>
+          <div className="flex justify-center items-center">
+               <h2 className="text-2xl font-extrabold text-salud-black" style={{fontFamily: 'Roboto'}}>Hola Invitado!</h2>
+               <Link href="/auth/signin">
+               <a className="text-xl stroke-black hover:text-salud-accent2 text-salud-accent mt-16 absolute font-bold" style={{fontFamily:'Roboto'}}>Inicia sesión</a>
+               </Link>
+               {/* <h2 className="mt-16 absolute" style={{fontFamily:'Roboto'}} > Puedes obtener estrellas</h2>
+                <h2 className="mt-28 absolute" style={{fontFamily:'Roboto'}} > y canjearlas por productos</h2> */}
+               </div>
+               
               </div>
-            </div>
-            <div className="py-6 px-5">
-              <div className="grid grid-cols-2 gap-4">
-                {navigation.pages.map((page) => (
-                  <Link key={page.name} href={page.href}>
-                    <a className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700">
-                      {page.name}
-                    </a>
-                  </Link>
-                ))}
               </div>
-            </div>
-          </div>
-        </Popover.Panel>
-      </Transition>
+        
     </Popover>
   );
 };
---
+++ components/users/avatar.js
@@ -0,0 +1,42 @@
+
+import Badge from '@mui/material/Badge';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+
+
+const SmallAvatar = styled(Avatar)(() => ({
+    width: 50,
+    height: 50,
+  
+  }));
+  
+
+const DefAvatar = (props) => {
+    console.log(props)
+    const random = Math.random() < 0.5 ? 'micah' : 'avataaars';  
+
+
+    return (
+        <Stack direction="row" spacing={2}>
+     
+        {/* <Badge
+          overlap="circular"
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+          style={{   
+                     
+         }}  
+          badgeContent={
+            <SmallAvatar alt="Remy Sharp" src="/badges/bronce.png" />
+          }
+        > */}
+          <Avatar alt="Travis Howard" style={ {width:100, height:100, border: `7px solid ${props.color? props.color : '#F0F0F0'}`,
+           borderRadius: `100%`,
+           width: `100px`,
+           height: `100px`,} } src={`https://avatars.dicebear.com/api/${random}/unknow.svg?background=%23000000`} />
+        {/* </Badge> */}
+      </Stack>
+    )
+}
+
+export default DefAvatar;
