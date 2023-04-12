7354455ac8493f02bc08e86c456d2ec8b2e7bfe3
KiraBelak <kalebchivas@hotmail.com>
2023-04-12 17:04:13 -0600

    camaras

--- components/camaron.js
+++ components/camaron.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Camera from 'react-camera';
 import { useRouter } from 'next/router';
 
-const Camara = () => {
+const CameraPage = () => {
   const [isCameraOpen, setIsCameraOpen] = useState(true);
   const router = useRouter();
 
@@ -18,31 +18,10 @@ const Camara = () => {
     <div className="min-h-screen flex justify-center items-center">
       <div className="max-w-md w-full">
         {isCameraOpen ? (
-          <div className="relative">
-            <Camera
-              onTakePhoto={(dataUri) => handleTakePhoto(dataUri)}
-              idealFacingMode={window && window.innerWidth > 768 ? 'environment' : 'user'}
-            />
-            <div className="absolute bottom-0 left-0 right-0 p-4 flex justify-center">
-              <button
-                onClick={() => setIsCameraOpen(false)}
-                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-4"
-              >
-                Cancelar
-              </button>
-              <button
-                onClick={() => {
-                  const canvas = document.querySelector('.camera canvas');
-                  if (canvas) {
-                    handleTakePhoto(canvas.toDataURL('image/jpeg', 1.0));
-                  }
-                }}
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              >
-                Tomar foto
-              </button>
-            </div>
-          </div>
+          <Camera
+            onTakePhoto={(dataUri) => handleTakePhoto(dataUri)}
+            idealFacingMode={window && window.innerWidth > 768 ? 'environment' : 'user'}
+          />
         ) : (
           <button
             onClick={() => setIsCameraOpen(true)}
@@ -56,4 +35,4 @@ const Camara = () => {
   );
 };
 
-export default Camara;
+export default CameraPage;
