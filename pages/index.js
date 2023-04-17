2a2ec0803245bf90685e7d0f92bfe25c9ac8a450
KiraBelak <kalebchivas@hotmail.com>
2023-04-17 14:18:59 -0600

    inicio de sesion y admin

--- pages/index.js
+++ pages/index.js
@@ -112,7 +112,12 @@ export default function Home({ isConnected }) {
   return (
     <MainLayout>
       <Toaster position="bottom-center" />
-      {abre ? (
+      {!session ? (
+        <div className="flex flex-col items-center justify-center">
+          Necesitas iniciar sesion para interactuar
+          </div>
+          ) : (
+            abre ? (
         <div onClick={handleOpen} className="flex min-h-[15px] justify-center bg-salud-primary rounded-b-3xl space-x-4 text-white">
           <svg width="24" className="w-1/6" height="50" viewBox="0 0 24 13" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M12.0001 12.17C11.3431 12.1712 10.6922 12.0429 10.0848 11.7923C9.4774 11.5418 8.92531 11.174 8.46012 10.71L0.290124 2.54002C0.101821 2.35172 -0.00396729 2.09632 -0.00396729 1.83002C-0.00396729 1.56372 0.101821 1.30832 0.290124 1.12002C0.478428 0.931716 0.733823 0.825928 1.00012 0.825928C1.26643 0.825928 1.52182 0.931716 1.71012 1.12002L9.88012 9.29002C10.4426 9.85182 11.2051 10.1674 12.0001 10.1674C12.7951 10.1674 13.5576 9.85182 14.1201 9.29002L22.2901 1.12002C22.4784 0.931716 22.7338 0.825928 23.0001 0.825928C23.2664 0.825928 23.5218 0.931716 23.7101 1.12002C23.8984 1.30832 24.0042 1.56372 24.0042 1.83002C24.0042 2.09632 23.8984 2.35172 23.7101 2.54002L15.5401 10.71C15.0749 11.174 14.5228 11.5418 13.9154 11.7923C13.308 12.0429 12.6572 12.1712 12.0001 12.17Z" fill="#fff" />
@@ -122,7 +127,7 @@ export default function Home({ isConnected }) {
 
       ) : (
         <div className="flex justify-center bg-salud-primary rounded-b-3xl space-x-4 text-white">
-{/* 
+
           {session.user.roles.includes("admin")&&(
             <div className="flex my-2 flex-col items-center space-y-1 text-white cursor-pointer group">
                             <div className="w-12 h-12 my-2 p-1 text-white">
@@ -136,7 +141,7 @@ export default function Home({ isConnected }) {
             </div>
             </div>
           )
-          } */}
+          }
           {menu.map((item, index) => {
             return (
               <div key={index} className="flex my-2 flex-col items-center space-y-1 text-white cursor-pointer group">
@@ -164,7 +169,9 @@ export default function Home({ isConnected }) {
           })
           }
         </div>
+      )
       )}
+
       {post ? (
         <div className="flex justify-center">
           <div className="w-full max-w-2xl">
